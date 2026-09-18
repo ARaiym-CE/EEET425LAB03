d0f2e0ae3728a495ca5a358975d8da6629ad898d
Ali Raiymkulov (RIT Student) <ar5310@rit.edu>
2026-09-18 11:48:33 -0400

    bruhhhh

--- lab03q11.m
+++ lab03q11.m
@@ -7,4 +7,31 @@ b = [1];
 
 y = filter(b, a, w);
 
-stem(w, y, 'filled')
+%stem(w, y, 'filled')
+
+ylength = length(y);
+m = -150:150;
+mlength = length(m);
+ryy = zeros(mlength, 1);
+rww = zeros(mlength, 1);
+
+for i = 1:mlength
+    lag = m(i);
+    if lag >= 0
+        ryy(i) = sum(y(1+lag:N) .* y(1:N-lag));
+    else
+        ryy(i) = sum(y(1:N+lag) .* y(1-lag:N));
+    end
+end
+    
+for i = 1:mlength
+    lag = m(i);
+    if lag >= 0
+        rww(i) = sum(w(1+lag:N) .* w(1:N-lag));
+    else
+        rww(i) = sum(w(1:N+lag) .* w(1-lag:N));
+    end
+end
+
+%stem(m, ryy, 'filled')
+stem(m, rww, 'filled')
